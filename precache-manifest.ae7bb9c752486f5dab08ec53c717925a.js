8a40c2fc45cd6a9b4764a78df556c396fb69c0e1
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-06-18 20:44:21 -0500

    Updates

---
+++ precache-manifest.ae7bb9c752486f5dab08ec53c717925a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f2f2cb7b54d9fec0c8170bc2b392c3",
+    "url": "/BW--Game-of-Life/index.html"
+  },
+  {
+    "revision": "b2f94bcb993fc03ada0a",
+    "url": "/BW--Game-of-Life/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f19e4bb89e42dd7b1d9",
+    "url": "/BW--Game-of-Life/static/js/2.deee3eea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BW--Game-of-Life/static/js/2.deee3eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f94bcb993fc03ada0a",
+    "url": "/BW--Game-of-Life/static/js/main.4947e08b.chunk.js"
+  },
+  {
+    "revision": "467bf598ff1ea991cd43",
+    "url": "/BW--Game-of-Life/static/js/runtime-main.cb50bfdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BW--Game-of-Life/static/media/logo.5d5d9eef.svg"
+  }
+]);
