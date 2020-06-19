8394fceeced0b52b412b2974122c9e7f9aa96543
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-06-18 21:53:00 -0500

    Played around with Canvas made my first 25 * 25 box

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+    const canvasi = useRef()
+
+    useEffect(()=> {
+        /**
+         * @type {HTMLCanvasElement} canvasi.current
+         */
+        const context = canvasi.current.getContext("2d")
+
+        for(let i = 0; i < 25; i ++){
+            //context.fillRect(i*10, 20, 10, 10);
+            for (let z = 0; z < 25; z ++){
+                context.fillRect(i*10, z*10, 10, 10);
+            }
+        }
+    })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <canvas ref={canvasi} width={"300px"} height={"450px"}/>
+
         <a
           className="App-link"
           href="https://reactjs.org"
